0d3cb93566302f18ab0e31c472db9f09290df7d4
Santiago Zarate <santiago@zarate.co>
2024-01-03 02:44:47 +0100

    Add very rudimentary argument parser
    - Encapsulate the printing and debugging of arguments inside
      debug_assertions

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,32 @@ use hetzner::*;
 use std::io::Write;
 
 fn main() {
+
+    if cfg!(debug_assertions) {
+        // TODO: add a command line argument parser
+        // main should parse cli arguments using getopts
+        // or its rust equivalent
+
+        use std::env;
+        // TODO: replace dbg with debug! from log crate
+        let arguments: Vec<String> = env::args().collect();
+        dbg!("args: {:?}", arguments.clone());
+
+        // Prints each argument on a separate line
+        arguments.iter().for_each(|argument| {
+            println!("{}", argument);
+        });
+    }
+
     let client_instance = HetznerClient::default();
+    // this could be changed in the future when multiple commands are supported
+    // it could be abuilder pattern like:
+    // let client_instance = HetznerClient::default().with_command("zones");
+    // another option would be to have a separate struct for each command
+    // and then have a function that takes a command struct and returns the
+    // response object
+    // let client_instance = HetznerClient::default().with_command(ZonesCommand);
+    // let client_instance = HetznerClient::default().with_command(RecordsCommand);
     let zones = query_zones(client_instance);
     process_zones(zones);
 }
@@ -23,7 +48,6 @@ fn process_zones(response_object: VecZoneRecord) {
         .expect(format!("failed to write zone_id {} to stdout", zone_id).as_str());
     }
     stdout.write_all(b"\n").expect("failed to write to stdout");
-    ()
 }
 
 #[cfg(test)]
