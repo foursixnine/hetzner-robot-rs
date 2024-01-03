ca8703e0cd3267e1f89109a757faa40c4029625a
Santiago Zarate <santiago@zarate.co>
2024-01-03 02:47:30 +0100

    Rename process_zones to display_zones

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,10 @@ fn main() {
     // let client_instance = HetznerClient::default().with_command(ZonesCommand);
     // let client_instance = HetznerClient::default().with_command(RecordsCommand);
     let zones = query_zones(client_instance);
-    process_zones(zones);
+    display_zones(zones);
 }
 
-fn process_zones(response_object: VecZoneRecord) {
+fn display_zones(response_object: VecZoneRecord) {
     let zones = response_object.zones;
     let mut stdout = std::io::stdout();
     for zone in zones {
@@ -55,8 +55,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process_zones_no_records() {
+    fn test_display_zones_no_records() {
         let response_object = VecZoneRecord { zones: vec![] };
-        assert_eq!(process_zones(response_object), ());
+        assert_eq!(display_zones(response_object), ());
     }
 }
