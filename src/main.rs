6b02e472dfa41de3b6dfab0fdbce3b82729a4a7d
Santiago Zarate <santiago@zarate.co>
2024-02-11 20:10:17 +0200

    Add command line argument parsing via clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,65 @@
 mod client;
 mod hetzner;
+use clap::{Parser, Subcommand};
 use client::*;
 use hetzner::*;
 use std::env;
+use std::fmt::{self, Debug, Formatter};
 use std::io::Write;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser, Clone)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    Zones {
+        #[arg(short, long)]
+        query: bool,
+    },
+}
+
+impl Debug for Commands {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Commands::Test { list } => write!(f, "Test {{ list: {} }}", list),
+            Commands::Zones { query } => write!(f, "Zones {{ query: {} }}", query),
+        }
+    }
+}
+
+impl Clone for Commands {
+    fn clone(&self) -> Self {
+        match self {
+            Commands::Test { list } => Commands::Test { list: *list },
+            Commands::Zones { query } => Commands::Zones { query: *query },
+        }
+    }
+}
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
+    let arguments = Cli::parse();
     if cfg!(debug_assertions) {
         // TODO: add a command line argument parser
         // main should parse cli arguments using getopts
@@ -14,16 +67,10 @@ fn main() {
         // The popular crate for this is clap
 
         // TODO: replace dbg with debug! from log crate
-        dbg!("args: {:?}", arguments.clone());
-
-        // Prints each argument on a separate line
-        arguments.iter().for_each(|argument| {
-            println!("{}", argument);
-        });
+        dbg!("args: {:?}", &arguments);
     }
 
     let client_instance = client::HetznerClient {
-        arguments: arguments.clone(),
         ..Default::default()
     };
     // this could be changed in the future when multiple commands are supported
@@ -34,8 +81,20 @@ fn main() {
     // response object
     // let client_instance = HetznerClient::default().with_command(ZonesCommand);
     // let client_instance = HetznerClient::default().with_command(RecordsCommand);
-    let zones = query_zones(client_instance);
-    display_zones(zones);
+
+    match arguments.command {
+        Some(Commands::Zones { query }) => {
+            if query {
+                let zones = query_zones(client_instance);
+                display_zones(zones);
+            } else {
+                let _ = std::io::stdout().write_all(b"No query to be performed");
+            }
+        }
+        _ => {
+            dbg!("args: {:?}", arguments);
+        }
+    }
 }
 
 fn display_zones(response_object: VecZoneRecord) {
